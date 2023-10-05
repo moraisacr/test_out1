1932d776a9e7c8174a772d26376e38cb43deda67
Augusto Cesar <66408298+moraisacr@users.noreply.github.com>
2023-10-05 07:51:36 -0300

    Update app_decoration.dart

--- lib/theme/app_decoration.dart
+++ lib/theme/app_decoration.dart
@@ -44,10 +44,4 @@ double get strokeAlignCenter => BorderSide.strokeAlignCenter;
 
 double get strokeAlignOutside => BorderSide.strokeAlignOutside;
 
-// For Flutter SDK Version 3.7.1 or less.
 
-// StrokeAlign get strokeAlignInside => StrokeAlign.inside;
-//
-// StrokeAlign get strokeAlignCenter => StrokeAlign.center;
-//
-// StrokeAlign get strokeAlignOutside => StrokeAlign.outside;
